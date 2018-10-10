c2ff6c3a62eb470db6ad44c34d07f2907a89bfee
Emma Neeson <emmaneesoneire@gmail.com>
2018-10-10 15:41:40 +0100

    Unity
    GameTools Animation

---
+++ GameTwools_Project1/Assets/UserControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControl : MonoBehaviour
+{
+
+    private float m_turn;
+    private float m_forward;
+
+    private character m_character;
+
+    private void Start()
+    {
+        m_character = GetComponent<character>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        m_turn = Input.GetAxis("Horizontal");
+        m_forward = Input.GetAxis("Vertical");
+        m_character.Move(m_turn, m_forward);
+    }
+}
---
+++ GameTwools_Project1/Assets/character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    private Animator m_animator;
+    
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void Move(float turn, float forward)
+    {
+        m_animator.SetFloat("Turn", turn);
+        m_animator.SetFloat("Forward", forward);
+    }
+}
